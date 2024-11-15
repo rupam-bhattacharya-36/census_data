cf36c6336774bf904f79b123f1cec9cbdcce4adb
Rupam Bhattacharyya <rupam.4815@outlook.com>
2024-11-15 14:29:20 +0530

    changed

---
+++ credentials.py
@@ -0,0 +1,13 @@
+mysql_cred={
+  'host' : "gateway01.ap-southeast-1.prod.aws.tidbcloud.com",
+  'port' : 4000,
+  'user' : "3NiG6FaS2x53Eyt.root",
+  'password' : "rPO54QQyqPSOvQfX",
+  'database' : "censusdb",
+}
+
+mongo_cred={
+    'url':'mongodb+srv://rupambhattacharya693:rupambhatt@cluster0.adijm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    'db':'census',
+    'collection':'census_collection'
+}
